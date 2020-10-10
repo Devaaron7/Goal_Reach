712540765e91ad09d6b22389e7908f28fc498037
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2020-10-09 20:06:03 -0400

    Update Start_main.py

--- Start_main.py
+++ Start_main.py
@@ -1,5 +1,7 @@
 import time
 
+goals = {}
+
 def intro():
     print("Welcome to A New Month - Lets set some goals")
     time.sleep(2)
@@ -34,6 +36,27 @@ def intro():
     time.sleep(2)
     return w_time
 
+def add_goal():
+    goal = input("Type the goal you would like to work on this month - ex. Complete a new book, or Head to the Gym twice a week. \n")
+    time.sleep(2)
+    time = int(input("Type the the hours needed to complete this goal - ex. A 300 page book will take me 30 hours to complete, or I spend 1.5 hours at the Gym, so 3 hours a week if I go twice. \n"))
+    time.sleep(2)
+    goals[goal] = time
+    time.sleep(2)
+    print("Goal added to log")
+
 intro()
 
 print("Great, ok - lets start assigning some goals to complete") 
+
+print(goals)
+
+add_goal()
+
+print(goals)
+
+add_goal()
+
+print(goals)
+
+
