8d1f65584491c9de4e0d4c6c8cfcb18d85eeeb77
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2020-10-11 17:15:19 -0400

    Progress
    Math inside the add goal function is off - the for loop is subtracting current values in goals dictionary for remaining hours every time, throwing off the numbers. Simple fix when I have time

--- Start_main.py
+++ Start_main.py
@@ -1,8 +1,7 @@
-import time
+import time, os
 
-goals = {}
-
-w_time = 100
+def clear():
+    os.system( "cls" )
 
 def intro():
     print("Welcome to A New Month - Lets set some goals")
@@ -36,11 +35,13 @@ def intro():
     time.sleep(2)
     print("Thats", m_time, " hours for the month.") 
     time.sleep(2)
-    return w_time
+    return m_time
 
 def add_goal():
+    global time_balance
     running = True
     while running:
+        print(goals)
         goal = input("""Type the goal you would like to work on this month
          - ex. Complete a new book, or Head to the Gym twice a week. \n""")
         time.sleep(2)
@@ -49,31 +50,37 @@ def add_goal():
           at the Gym, so 3 hours a week if I go twice. \n"""))
         time.sleep(2)
         goals[goal] = duration
+        clear()
         time.sleep(2)
         print("Goal added to log")
 
         for x in goals.values():
-            w_time -= x
+            time_balance -= x
         
-        print("Remaining amount of time for month: ", w_time)
-        if w_time <= 3:
-            print("We're running out of available time for anymore goals --- current balance is: ", w_time)
+        print("Remaining amount of time for month: ", time_balance)
+        if time_balance <= 3:
+            print("We're running out of available time for anymore goals --- current balance is: ", time_balance)
             running = False
         else:
             print("Do you want to add another goal for the month? Type 'y' for yes or 'n' for no \n")
             choice = input()
             if choice.lower == "y":
+                clear()
                 pass
             elif choice.lower == "n":
                 running = False
 
-#intro()
+goals = {}
+
+time_balance = intro() 
+
+clear()
 
 print("Great, ok - lets start assigning some goals to complete") 
 
-print(goals)
+print(time_balance)
 
 add_goal()
 
-
+input("Program Ended")
 
