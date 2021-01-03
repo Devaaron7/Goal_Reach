0ab8dcf9ab296ab03e99cdfae6b3b19a90e05966
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-01-03 14:17:30 -0500

    Shortening text code
    compressing text code in the html generator project. Shorten wizard hours

--- End_2.py
+++ End_2.py
@@ -10,36 +10,13 @@ def intro():
     clear()
     print("Welcome to A New Month - Lets set some goals")
     time.sleep(2)
-    w_time = 168
-    print("Everyone has the same amount of time")
-    time.sleep(2)
-    print("It's how we spend it that makes us different")
-    time.sleep(2)
-    print("First, lets see how much time we have for our goals")
-    time.sleep(3)
     clear()
-    print("Current Unallocated time: ", w_time)
-    work_time = int(input("Enter how many hours a week you spend working \n"))
-    w_time -= work_time
-    time.sleep(2)
-    print("Current Unallocated time: ", w_time)
-    time.sleep(2)
-    sleep_time = int(input("Enter how many hours of sleep on average do you get a night \n")) * 7
-    w_time -= sleep_time
-    time.sleep(2)
-    print("Current Unallocated time: ", w_time)
-    time.sleep(2)
-    human_time = w_time / 3
-    w_time -= round(human_time, -1)
+    #print("Current Unallocated time: ", w_time)
+    work_time = int(input("How many hour per week do you want to use for your Goals? \n"))
+    w_time = 0
+    w_time += work_time
     clear()
-    print("While it's great to try and be perfect, shit happens.")
-    time.sleep(2)
-    print("We'll deduct a fraction of available time and chalk it up to being human.")
-    time.sleep(2)
-    print("Current Unallocated time: ", w_time)
-    time.sleep(2)
-    clear()
-    print("""Ok, so weekly, after deducting Work, Sleep, and some padding - You have""", w_time,""" hours left to work on your goals.""")
+    print("""Ok, so weekly, You have""", w_time,""" hours available to work on your goals.""")
     m_time = w_time * 4
     time.sleep(2)
     clear()
@@ -101,9 +78,8 @@ def ending():
     time.sleep(2)
     print("Nice work!!")
 
-#time_balance = intro() 
+time_balance = intro() 
 
-time_balance = 100
 clear()
 
 print("Ok - lets start assigning some goals to complete") 
--- p_2_h.py
+++ p_2_h.py
@@ -1,188 +1,139 @@
 import os
-from Mid import goals
-'''
-Logic to display correct Hours / Minutes for Divided Goal Time
-
-# Divide into 4 with hours and minutes
-
-## 10 - 2 Hours & 30 Minutes outputted
-
-i = 7.5 # Hours
-
-m = i * 60  # minutes
-
-d = m / 4
-
-re_hr = d // 60
-
-re_min = d % 60
-
-if re_min == 0:
-    print("If divided by 4, it was take you {} hour(s) to complete".format(int(re_hr)))
-elif re_hr == 0:
-    print("If divided by 4, it was take you {} minutes to complete".format(int(re_min)))
-else:
-    print("If divided by 4, it was take you {} hours & {} minutes to complete".format(int(re_hr), int(re_min)))
-
-'''
-
+from End_2 import goals
 
 t = goals
+'''
+def freq_text():
+    body = """
+    <label class="control control-checkbox">
+    {sg}
+    <input type="checkbox" />
+    <div class="control_indicator"></div>
+    </label>
+    <label class="control control-checkbox">
+    """
+    return body
+'''
+
+def html_body1():
+    body = """
+
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <style>
+
+    .control {
+    font-family: arial;
+    display: block;
+    position: relative;
+    padding-left: 36px;
+    margin-bottom: 15px;
+    padding-top: 3px;
+    cursor: pointer;
+    font-size: 17px;
+    }
+    .control input {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+    }
+    .control_indicator {
+    position: absolute;
+    top: 2px;
+    left: 0;
+    height: 20px;
+    width: 20px;
+    background: #e6e6e6;
+    border: 1px solid #000000;
+    border-radius: 0px;
+    }
+    .control:hover input ~ .control_indicator,
+    .control input:focus ~ .control_indicator {
+    background: #cccccc;
+    }
+
+    .control input:checked ~ .control_indicator {
+    background: #2aa1c0;
+    }
+    .control:hover input:not([disabled]):checked ~ .control_indicator,
+    .control input:checked:focus ~ .control_indicator {
+    background: #0e6647d;
+    }
+    .control input:disabled ~ .control_indicator {
+    background: #e6e6e6;
+    opacity: 0.6;
+    pointer-events: none;
+    }
+    .control_indicator:after {
+    box-sizing: unset;
+    content: '';
+    position: absolute;
+    display: none;
+    }
+    .control input:checked ~ .control_indicator:after {
+    display: block;
+    }
+    .control-checkbox .control_indicator:after {
+    left: 8px;
+    top: 4px;
+    width: 3px;
+    height: 8px;
+    border: solid #ffffff;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+    }
+    .control-checkbox input:disabled ~ .control_indicator:after {
+    border-color: #7b7b7b;
+    }
+
+    {display: inline-block; *display: inline; zoom: 1; vertical-align: top; font-size: 12px;}
 
 
-
-paste_start = """
-
-<!DOCTYPE html>
-<html>
-<body>
-
-<style>
-
-.control {
-font-family: arial;
-display: block;
-position: relative;
-padding-left: 36px;
-margin-bottom: 15px;
-padding-top: 3px;
-cursor: pointer;
-font-size: 17px;
-}
-.control input {
-position: absolute;
-z-index: -1;
-opacity: 0;
-}
-.control_indicator {
-position: absolute;
-top: 2px;
-left: 0;
-height: 20px;
-width: 20px;
-background: #e6e6e6;
-border: 1px solid #000000;
-border-radius: 0px;
-}
-.control:hover input ~ .control_indicator,
-.control input:focus ~ .control_indicator {
-background: #cccccc;
-}
-
-.control input:checked ~ .control_indicator {
-background: #2aa1c0;
-}
-.control:hover input:not([disabled]):checked ~ .control_indicator,
-.control input:checked:focus ~ .control_indicator {
-background: #0e6647d;
-}
-.control input:disabled ~ .control_indicator {
-background: #e6e6e6;
-opacity: 0.6;
-pointer-events: none;
-}
-.control_indicator:after {
-box-sizing: unset;
-content: '';
-position: absolute;
-display: none;
-}
-.control input:checked ~ .control_indicator:after {
-display: block;
-}
-.control-checkbox .control_indicator:after {
-left: 8px;
-top: 4px;
-width: 3px;
-height: 8px;
-border: solid #ffffff;
-border-width: 0 2px 2px 0;
-transform: rotate(45deg);
-}
-.control-checkbox input:disabled ~ .control_indicator:after {
-border-color: #7b7b7b;
-}
-
-{display: inline-block; *display: inline; zoom: 1; vertical-align: top; font-size: 12px;}
+    .weektitle {
+    width: 100%;
+    float: left;
+    padding: 5px;
+    border: 0px solid red;
+    }
 
 
-.weektitle {
-width: 100%;
-float: left;
-padding: 5px;
-border: 0px solid red;
-}
+    .week1 {
+    width: 20%;
+    float: left;
+    padding: 5px;
+    border: 0px solid red;
+    }
+
+    .week2 {
+    width: 20%;
+    float: left;
+    padding: 5px;
+    border: 0px solid red;
+    }
+
+    .week3 {
+    width: 20%;
+    float: left;
+    padding: 5px;
+    border: 0px solid red;
+    }
 
 
-.week1 {
-width: 20%;
-float: left;
-padding: 5px;
-border: 0px solid red;
-}
+    .week4 {
+    width: 20%;
+    float: left;
+    padding: 5px;
+    border: 0px solid red;
+    }
 
-.week2 {
-width: 20%;
-float: left;
-padding: 5px;
-border: 0px solid red;
-}
+    </style>
 
-.week3 {
-width: 20%;
-float: left;
-padding: 5px;
-border: 0px solid red;
-}
-
-
-.week4 {
-width: 20%;
-float: left;
-padding: 5px;
-border: 0px solid red;
-}
-
-</style>
-
-"""
-
-paste_end = """
-
-</body>
-</html>
-
-"""
-
-Total_Paste = ""
-
-Total_Paste += paste_start
-
-
-for x in t.keys():
-
-    goal = x
-
-    i = t.get(x)
-
-    m = i * 60  # minutes
-
-    dd = m / 4
-
-    d = dd / 4
-
-    re_hr = d // 60
-
-    re_min = d % 60
-
-    if re_min == 0:
-        i = "Focus for {} hour(s)".format(int(re_hr))
-    elif re_hr == 0:
-        i = "Focus for {} minutes".format(int(re_min))
-    else:
-        i = "Focus for {} hours & {} minutes".format(int(re_hr), int(re_min))
-
-    paste2 = """
+    """
+    return body
+def html_body2():
+    body = """
 
     <div class="weektitle">
 
@@ -299,10 +250,42 @@ for x in t.keys():
 
     </div>
 
-    """.format(sg = i, g = goal)
+    """
+    return body
+
+paste_start = html_body1()
+paste_end = """
+</body>
+</html>
+"""
+Total_Paste = ""
+Total_Paste += paste_start
+
+for x in t.keys():
+    goal = x
+
+    i = t.get(x)
+
+    m = i * 60 
+
+    dd = m / 4
+
+    d = dd / 4
+
+    re_hr = d // 60
+
+    re_min = d % 60
+
+    if re_min == 0:
+        i = "Focus for {} hour(s)".format(int(re_hr))
+    elif re_hr == 0:
+        i = "Focus for {} minutes".format(int(re_min))
+    else:
+        i = "Focus for {} hours & {} minutes".format(int(re_hr), int(re_min))
+
+    paste2 = html_body2().format(sg = i, g = goal)
 
     Total_Paste += paste2
-
     Total_Paste += paste_end
 
 f = open("test.html", "w")
